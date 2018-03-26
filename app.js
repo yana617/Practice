0be78f0821bcae363634ff9e3037422c948d6274
Yana Sidorova <jana.ru.sidorova@yandex.ru>
2018-03-26 21:10:06 +0300

    small change :)

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ app.use('/public', router);
 app.get('/', (req, res) => res.sendFile(path.resolve('./public/UI/index.html')));
 
 const server = app.listen(3000, () => console.log(`Server is listening on port ${server.address().port}`));
+
+app.use((req, res) => {
+    res.sendFile(path.resolve('./public/UI/errorPage.html'));
+});
